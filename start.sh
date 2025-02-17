5d1ef50da610412c8f77c3d7a40cfbfb3db7a394
Orion codes 3 <the55thone@gmail.com>
2025-02-16 17:00:22 -0800

    automated update from gemini ai - modified start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
+```sh
+#!/bin/bash
+set -e
+
+# clean up process
+killall -9 python3
+killall -9 node
+
+# (re)build web application
 sh build.sh
+
+# start web application
 python3 webapp.py
+
+```
