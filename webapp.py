31352713990b17c39f326a1e2d91304ee79f2798
Orion codes 3 <the55thone@gmail.com>
2025-02-16 17:00:31 -0800

    automated update from gemini ai - modified webapp.py

--- webapp.py
+++ webapp.py
@@ -1,3 +1,4 @@
+```python
 from flask import Flask, render_template, request, jsonify
 from threading import Thread
 from kahoot import flood_bots
@@ -15,11 +16,39 @@ def ping_self():
     while True:
       requests.get("https://rattle-icy-limit.glitch.me/")
       time.sleep(5)
-  
-  
+
+# Sidebar links
+sidebar_links = [
+    {"name": "Home", "link": "/"},
+    {"name": "Games", "link": "/games"},
+    {"name": "Themes", "link": "/themes"},
+    {"name": "Particles", "link": "/particles"},
+]
+
+# Game cards
+game_cards = [
+    {"name": "Kahoot!", "image": "/static/images/kahoot.png", "link": "/kahoot"},
+    {"name": "Blooket", "image": "/static/images/blooket.png", "link": "/blooket"},
+    {"name": "Quizizz", "image": "/static/images/quizizz.png", "link": "/quizizz"},
+]
+
+# Themes
+themes = [
+    {"name": "Light", "link": "/light"},
+    {"name": "Dark", "link": "/dark"},
+    {"name": "Blue", "link": "/blue"},
+]
+
+# Particles
+particles = [
+    {"name": "Snow", "link": "/snow"},
+    {"name": "Rain", "link": "/rain"},
+    {"name": "Fire", "link": "/fire"},
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', sidebar_links=sidebar_links, game_cards=game_cards, themes=themes, particles=particles)
 
 
 @app.route('/start', methods=['POST'])
@@ -40,3 +69,4 @@ if __name__ == '__main__':
     ping_thread = Thread(target=ping_self)
     ping_thread.start()
     app.run(host='0.0.0.0', port=8080)
+```
