9e9643069f70d376001395c53a6c698a1e648a77
Orion codes 3 <the55thone@gmail.com>
2025-02-16 17:00:14 -0800

    automated update from gemini ai - modified build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
+```
+#!/bin/bash
+
+set -e
+
+# Install Python dependencies
 pip3 install -r python_requirements.txt
+
+# Install Node.js dependencies
 npm install kahoot.js-latest
+npm install ultraviolet
+npm install --save-dev gulp
+npm install --save-dev gulp-sass
+npm install --save-dev gulp-autoprefixer
+npm install --save-dev gulp-minify-css
+npm install --save-dev gulp-uglify
+npm install --save-dev gulp-imagemin
+npm install --save-dev gulp-rename
+npm install --save-dev gulp-concat
+npm install --save-dev gulp-sourcemaps
+
+# Build the game site
+gulp build
+```
