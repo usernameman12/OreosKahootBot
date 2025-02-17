1cad75ab195779b61515bce62a09f4ea6b565e28
Orion codes 3 <the55thone@gmail.com>
2025-02-16 17:08:29 -0800

    automated update from gemini ai - modified start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ sh build.sh
 # Serve themes and particles
 sh serve_theme_particles.sh &
 
-# Start the ultraviolet proxy
-run_proxy.sh &
+# Start the ultraviolet proxy with custom config
+run_proxy.sh -c ultraviolet_custom_config.yml &
 
 # Start the game site
 sh start_proc.sh
